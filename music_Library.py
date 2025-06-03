778ed37e7735f293d17a05a590962245da92e2dc
Rohit-pro10 <rohitgavali790@gmail.com>
2025-06-03 17:20:34 +0530

    Add files via upload

---
+++ music_Library.py
@@ -0,0 +1,6 @@
+
+music = {
+ "sky" : "https://youtu.be/BBAyRBTfsOU?si=EyowCvBb6sFrNGBx",
+ "kali"  : "https://youtu.be/o-7b6ctrQX0?si=MXLJST_nQWJAvXZl",
+ "fitur" : "https://youtu.be/IFGM4pOPXvA?si=qNktUq4Lf-gJxpfr"
+}
